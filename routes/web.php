31164ed50c4823ec5d377ca8e738d867c9a9a91e
jrarkaan <rakajanitraa@hotmail.com>
2021-05-26 18:07:15 +0700

    coding enhjoy

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get("/siswa", "SiswaController@index");
+Route::post("/siswa/create", "SiswaController@create");
+Route::get('/siswa/{id}/edit', "SiswaController@edit");
+Route::post('/siswa/{id}/update', "SiswaController@update");
